83760b9be6b2c09b8189af04a934bd2c10a1e144
Stanley5051 <timofeev-egor90@yandex.ru>
2024-09-16 11:08:05 +0300

    Исполнение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        // Получаем два бина HelloWorld
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        // Сравнение по ссылке для HelloWorld (должно быть true)
+        System.out.println("HelloWorld бин1 равен бин2? " + '\n'+(bean1 == bean2));
+
+        // Получаем два бина Cat
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        // Сравнение по ссылке для Cat (должно быть false, так как бин prototype)
+        System.out.println("Cat бин1 равен бин2? " + '\n'+(cat1 == cat2));
     }
 }
